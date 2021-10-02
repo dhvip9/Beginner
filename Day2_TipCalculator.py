a90ca2f62d1e7fa90207a223290d399c76678c97
DhvipPatel <88395827+dhvip9@users.noreply.github.com>
2021-10-02 15:14:47 +0530

    Day2 Final Project

---
+++ Day2_TipCalculator.py
@@ -0,0 +1,5 @@
+total_amount = float(input("Welcome to the tip Calculator\nWhat was the total bill?\n>> "))
+perc_tip = float(input("What percentage tip would you like to give?[ 10%, 12%, or 15% ]\n>> "))
+total_person = float(input("How person should pay?\n>> "))    
+payable_amt = ( total_amount + ( total_amount * perc_tip / 100 ) ) / total_person   
+print(f"Each person should pay:[ {round(payable_amt,2)} ]")
