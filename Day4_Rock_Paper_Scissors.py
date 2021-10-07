01ba3a1a3979b21287a5c1b12b07dc715a4fc941
DhvipPatel <88395827+dhvip9@users.noreply.github.com>
2021-10-07 16:35:50 +0530

    Update Day4_Rock_Paper_Scissors.py

--- Day4_Rock_Paper_Scissors.py
+++ Day4_Rock_Paper_Scissors.py
@@ -28,39 +28,59 @@ def pick(number):
           (____)
     ---.__(___)
     '''
+  else:
+    choice = """  [ Incorrect Choice! ]
+-------------------------"""  
   return choice
+ 
+
 
 
 def winner(player, computer):
   win = ""
   if player == 2 and computer == 1:
-    win = "YOU WIN!"
+    win = "[ YOU WIN! ]"
   elif player == 1 and computer == 2:
-    win = "YOU LOSE!"
+    win = "[ YOU LOSE! ]"
 
   elif player == 1 and computer == 0:
-    win = "YOU WIN!" 
+    win = "[ YOU WIN! ]" 
   elif player == 0 and computer == 1:
-    win = "YOU LOSE!"  
+    win = "[ YOU LOSE! ]"  
 
   elif player == 0 and computer == 2:
-    win = "YOU WIN!"  
+    win = "[ YOU WIN! ]"  
   elif player == 2 and computer == 0:
-    win = "YOU LOSE!"  
+    win = "[ YOU LOSE! ]"
+
+  else:
+    win = "[ DRAW! ]"    
   
   return win 
 
 
 print(" |                What Do You Choose?                 | ")
 print(" TYPE [ 0 for ROCK ] [ 1 for PAPER ] [ 2 for SCISSOR ]  \n")
+print("Exit from 'RPS' Write [ -1 ]")
+# player = int(input(f"Enter Number\n>> "))
 
-player = int(input(">> "))
-print("Player Choice")  
-print(pick(player)+"\n\n")
+while True:
+  player = int(input(f"Enter Number\n>> "))
+  if player == -1:
+    break
+  while True:
+    print()
+    print("Player Choice")  
+    print(pick(player), "\n\n")
 
-print("Computer Choice")
-computer = randint(0, 2)
-print(pick(computer))
- 
-print(winner(player, computer)) 
+    if player >= 3 or player <= -1:
+      player = int(input(f"Enter Number\n>> "))
+      
+    else:  
+      print("Computer Choice")
+      computer = randint(0, 2)
+      print(pick(computer))
 
+      print(winner(player, computer))
+      print("----------------------\n")
+      break
