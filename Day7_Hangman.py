a0d8eb4e6cec2ef38171c77ca0bbed306c9abdfe
DhvipPatel <88395827+dhvip9@users.noreply.github.com>
2021-10-21 14:48:53 +0530

    final project

--- Day7_Hangman.py
+++ Day7_Hangman.py
@@ -1,66 +1,129 @@
-from random import randint 
+import random  
 
-def if_false(life):
-  if life == 1:
-    print("  +---+  ")
-    print("  |   |  ")
-    print("  o   |  ")
-    print("      |  ")
-    print("      |  ")
-    print("      |  ")
-    print("      |  ")
-    print("=========")
-  elif life == 2:
-    print("  +---+  ")
-    print("  |   |  ")
-    print("  o   |  ")
-    print("  |   |  ")
-    print("      |  ")
-    print("      |  ")
-    print("      |  ")
-    print("=========")
-  elif life == 3:
-    print("  +---+  ")
-    print("  |   |  ")
-    print("  o   |  ")
-    print(" /|   |  ")
-    print("      |  ")
-    print("      |  ")
-    print("      |  ")
-    print("=========")
-  elif life == 4:
-    print("  +---+  ")
-    print("  |   |  ")
-    print("  o   |  ")
-    print(" /|\  |  ")
-    print("      |  ")
-    print("      |  ")
-    print("      |  ")
-    print("=========") 
-  elif life == 5:
-    print("  +---+  ")
-    print("  |   |  ")
-    print("  o   |  ")
-    print(" /|\  |  ")
-    print(" /    |  ")
-    print("      |  ")
-    print("      |  ")
-    print("=========") 
-  elif life == 6:
-    print("  +---+  ")
-    print("  |   |  ")
-    print("  o   |  ")
-    print(" /|\  |  ")
-    print(" / \  |  ")
-    print("      |  ")
-    print("      |  ")
-    print("=========") 
+def if_life(life):
+    if life == 5:
+      draw = '''
+          +---+  
+          |   |  
+          o   |  
+              |  
+              |  
+              |  
+              |  
+        =========
+            '''
+    elif life == 4:
+      draw = '''
+          +---+  
+          |   |  
+          o   |  
+          |   |  
+              |  
+              |  
+              |  
+        =========
+            '''
+    elif life == 3:
+      draw = '''
+          +---+  
+          |   |  
+          o   |  
+         /|   |  
+              |  
+              |  
+              |  
+        =========
+            '''
+    elif life == 2:
+      draw = '''
+          +---+  
+          |   |  
+          o   |  
+         /|\\  |  
+              |  
+              |  
+              |  
+        =========
+            '''
+    elif life == 1:
+      draw = '''
+          +---+  
+          |   |  
+          o   |  
+         /|\\  |  
+         /    |  
+              |  
+              |  
+        =========
+            '''
+    elif life == 0:
+      draw = '''
+          +---+  
+          |   |  
+          o   |  
+         /|\\  |  
+         / \\  |  
+              |  
+              |  
+        =========
+            '''
+    else:
+      draw = '''
+          +---+  
+          |   |  
+              |  
+              |  
+              |  
+              |  
+              |  
+        =========
+            '''         
+    return draw
 
+print('''
+ _                                             
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/      
+      ''')
 
+words = ["dhvip", "ardvark", "acknowledgement", "enclose", "baboon", "camel", "furore", "glamor", "judgement", "lambast", "lynchpin", "omelet", "theater", "woolen", "yoghurt"]
 
-spelling = ["dhvip", "heril", "kenil", "fenil", "meet"]
-for _ in range(len(spelling)):
-  index = randint(0, len(spelling) - 1)
-  spell = [i for i in spelling[index] ]
-  print(spell)
+spelling = list(random.choice(words))
+copy_spell = " ".join(spelling)
+# print(copy_spell)
 
+find_word = []
+for i in range(len(spelling)):
+  find_word.append("_")
+
+Life = 6
+while True:
+    user_letter = input("Guess a Letter :- ").lower()
+    Index = 0
+    if user_letter not in spelling:
+        print(f"You Guessed {user_letter}, That's Not in Word. You Lose a Life")
+        Life -= 1    
+    for i in spelling:
+      if i == user_letter:
+        Index = spelling.index(i)
+        spelling[Index] = "0"
+        find_word[Index] = i
+  
+    final_word = " ".join(find_word)
+    print(f"      [ {final_word} ]        ")
+
+    print(if_life(Life))
+  
+    if final_word == copy_spell:
+      print("[ YOUR MAN SAVED! ]")
+      break
+    elif Life == 0:
+      print("[ YOUR MAN DIED! ]")
+      break
+      
+    print("+--------------------------------------------------+")
