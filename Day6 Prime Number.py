471c9157e772e24f7b4ff47ce5a66959389ca527
DhvipPatel <88395827+dhvip9@users.noreply.github.com>
2021-10-23 12:37:49 +0530

    final project

---
+++ Day6 Prime Number.py
@@ -0,0 +1,12 @@
+def prime_checker(number):
+	prime = 0
+	for i in range(2, number):
+		if number % i == 0:
+			prime += 1
+	if prime > 1:
+		print("It's not a prime number.")
+	else:
+		print("It's a prime number.")
+    
+n = int(input("Check this number: "))
+prime_checker(number=n)
